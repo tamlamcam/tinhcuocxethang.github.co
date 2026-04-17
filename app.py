004fc830099d85802f374b9ad3e8fabb432633e7
tamlamcam <onigiri92yen@gmail.com>
2026-04-17 11:13:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-st.set_page_config(page_title="Tính giá thuê xe", layout="centered")
+st.set_page_config(page_title="Tính giá thuê xe", layout="wide")
 
 st.title("🚗 Phần mềm tính giá thuê xe")
 
